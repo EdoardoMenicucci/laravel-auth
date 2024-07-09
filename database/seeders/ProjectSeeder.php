e27239f52f21215601715b357909bcf24513f661
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-07-09 17:06:08 +0200

    add faker on seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,20 +5,23 @@ namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $newProject = new Project();
-        $newProject->name = 'Project 1';
-        $newProject->description = 'Project 1 description';
-        $newProject->start_date = '1972-03-24';
-        $newProject->end_date = '1972-04-24';
-        $newProject->status = 1;
-        $newProject->save();
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->name = $faker->sentence(3);
+            $newProject->description = $faker->paragraph(2);
+            $newProject->start_date = $faker->date($format = 'Y-m-d', $max = 'now');
+            $newProject->end_date = $faker->date($format = 'Y-m-d', $max = 'now');
+            $newProject->status = $faker->numberBetween(0, 1);
+            $newProject->save();
+        }
     }
 }
