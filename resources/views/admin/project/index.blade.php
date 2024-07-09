35930e91096112929df6ee33170d961570263997
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-09 17:49:11 +0200

    project.index

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -1,29 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
-	<div class="container-fluid">
-		<div class="row justify-content-center pt-3">
-			<div class="card col-md-3 m-3">a</div>
-			<div class="card col-md-3 m-3">a</div>
-			<div class="card col-md-3 m-3">a</div>
-		</div>
-	</div>
-
 	<div class="container-fluid mt-4">
 		<div class="row justify-content-center">
 			@foreach ($projects as $project)
 				<div class="col-md-3 m-3">
 					<div class="card">
-						<div class="card-header">{{ $project->name }}</div>
-
+						<div class="card-header"><a href="{{ route('admin.project.show', $project) }}">{{ $project->name }}</a></div>
 						<div class="card-body">
-							{{ $project->description }}
-
+							<div>{{ $project->description }}</div>
+							<div>-Data inizio: {{ $project->start_date }}</div>
+							<div>-Data fine: {{ $project->end_date }}</div>
+							@if ($project->status)
+								<div class="green fw-bold">Completato</div>
+							@else
+								<div class="red fw-bold">Non Completato</div>
+							@endif
 						</div>
-
 					</div>
 				</div>
 			@endforeach
 		</div>
+
+	</div>
 	</div>
 @endsection
