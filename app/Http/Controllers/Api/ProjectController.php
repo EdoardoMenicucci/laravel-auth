55658f4a553f2766dde511baa95bdf5f3fb5e20a
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-22 15:29:04 +0200

    Collegamento tabella api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
 
     public function index()
     {
+        $projects = Project::with('type')->orderByDesc('id')->paginate(10);
+
         return response()->json([
             'success' => true,
             //::whit('technologies','type') se vuoi importare anche i collegamenti alle altre tabelle del database EAGER LOADING
-            'projects' => Project::orderByDesc('id')->paginate(10)
+            'projects' => $projects
         ]);
     }
 }
